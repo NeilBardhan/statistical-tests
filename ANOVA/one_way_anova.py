c338fedf7c0f3225ca6d0d4f0bad41c08c89521f
NeilBardhan <indraneil.bardhan@gmail.com>
2020-03-29 12:36:17 -0400

    Updated anova.py

--- ANOVA/one_way_anova.py
+++ ANOVA/one_way_anova.py
@@ -5,10 +5,29 @@ from scipy import stats
 os.chdir('..')
 path = os.getcwd()
 
-data_file = path + '\\data\\NMttest.csv'
-data = pd.read_csv(data_file, header = 'infer')
+def load_data(data_file):
+    return pd.read_csv(data_file, header = 'infer')
 
-anova = stats.f_oneway(data['shiftClass21'].dropna().values, data['shiftClass22'].dropna().values,
-               data['shiftClass23'].dropna().values, data['shiftClass31'].dropna().values,
-               data['shiftClass32'].dropna().values)
-print(anova)
+def anova_scipy(data):
+    xlist = []
+    for col in data:
+        xlist.append(data[col].dropna().values)
+    anova = stats.f_oneway(*xlist)
+    return anova
+
+def main():
+    data_file = path + '\\data\\NMttest.csv'
+    df = load_data(data_file)
+    alpha = 0.05
+    sig_flag = 0
+    anova0 = anova_scipy(df)
+    anova0_F = round(anova0[0], 4)
+    anova0_pval = round(anova0[1], 4)
+    if anova0_pval <= alpha:
+        sig_flag = 1
+    print("F Stat :  ", anova0_F)
+    print("P Value:  ", anova0_pval)
+    print("Significant Difference :  ", sig_flag)
+
+if __name__ == '__main__':
+    main()
