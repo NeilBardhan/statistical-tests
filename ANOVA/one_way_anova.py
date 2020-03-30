4ed7578814018ee9cc5148dd2bbe0d0efdd7aff5
NeilBardhan <indraneil.bardhan@gmail.com>
2020-03-30 13:03:42 -0400

    anova.py added manual computation

--- ANOVA/one_way_anova.py
+++ ANOVA/one_way_anova.py
@@ -1,12 +1,48 @@
 import os
 import pandas as pd
 from scipy import stats
+from statistics import mean
 
 os.chdir('..')
 path = os.getcwd()
 
 def load_data(data_file):
     return pd.read_csv(data_file, header = 'infer')
+
+def anova(data, alpha):
+    report = {}
+    ss_list = []
+    square_list = []
+    N = 0
+    k = 0
+    total_sum = 0
+    for col in data:
+        k += 1
+        col_mean = mean(data[col].dropna().values)
+        N += len(data[col].dropna().values)
+        total_sum += sum(data[col].dropna().values)
+        square_list.append(sum([i ** 2 for i in data[col].dropna().values]))
+        ss_list.append(sum([(i - col_mean) ** 2 for i in data[col].dropna().values]))
+    ssw = sum(ss_list)
+#    ssb = 
+    sst = sum(square_list) - ((total_sum ** 2)/N)
+    ssb = sst - ssw
+    dfb = k - 1
+    dfw = N - k
+    msb = ssb/dfb
+    msw = ssw/dfw
+    F = round(msb/msw, 4)
+    pvalue = round(stats.f.sf(F, dfb, dfw), 4)
+#    print(F, pvalue)
+    report["F_statistic"] = F
+    report["p_value"] = pvalue
+    if report["p_value"] <= alpha:
+        report["significance"] = 1
+        report["reject_H0"] = 1
+    else:
+        report["significance"] = 0
+        report["reject_H0"] = 0
+    return report
 
 def anova_scipy(data, alpha):    
     report = {}
@@ -34,6 +70,8 @@ def main():
     print("P Value:  ", anova_report["p_value"])
     print("Significant Difference :  ", anova_report["significance"])
     print("Reject the null hypothesis:  ", anova_report["reject_H0"])
+    anova_report = anova(df, alpha)
+    print(anova_report)
 
 if __name__ == '__main__':
     main()
